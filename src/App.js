5866db7ecf4144f2c16f5d535ae610c4e9099f8a
Mahmoud Shorafa <m2shorafa@gmail.com>
2022-02-04 19:37:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useMemo, useState } from "react";
+import { numberAndOperators } from "./help/numberAndOperators";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState("");
+
+  //function
+  const reset = () => {
+    setData("");
+  };
+  const result = () => {
+    setData((data) => eval(data));
+  };
+  const remove = () => {
+    setData((data) => data.toString().slice(0, -1));
+  };
+
+  //useMemo
+  const but = useMemo(() => {
+    return numberAndOperators.map((number, i) => (
+      <button key={i} onClick={() => setData((data) => data + number)}>
+        {number}
+      </button>
+    ));
+  }, []);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="bottom">
+          <form name="calc">
+            <input type="text" name="text" id="screen" value={data} readOnly />
+          </form>
+
+          <div className="buttn">
+            {but}
+            <button onClick={() => remove()} name="del" className="del">
+              del
+            </button>
+            <button onClick={() => reset()} className="reset">
+              reset
+            </button>
+            <button onClick={() => result()} className="equal">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
